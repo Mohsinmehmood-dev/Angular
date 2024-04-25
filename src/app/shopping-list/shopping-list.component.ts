dcfd80b7cb5231352f44461ed56330d98d8eb4d0
Mohsinmehmood-dev <mohsinmehmood.dev@gmail.com>
2024-04-25 23:13:56 +0500

    updated shopping list and edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { Ingredient } from '../shared/ingredient.model';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-shopping-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+    selector: 'app-shopping-list',
+    standalone: true,
+    templateUrl: './shopping-list.component.html',
+    styleUrl: './shopping-list.component.css',
+    imports: [ShoppingEditComponent , CommonModule]
 })
 export class ShoppingListComponent {
-
+  ingredients : Ingredient [] =[
+    new Ingredient("apple",10),
+    new Ingredient("mango",15),
+    new Ingredient("banana",3),
+    new Ingredient("strawberry",13),
+    new Ingredient("grape",7),
+  ]
 }
