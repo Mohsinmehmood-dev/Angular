1434124ea12eb095442a26e7e5c34df1b43bea00
Mohsinmehmood-dev <mohsinmehmood.dev@gmail.com>
2024-04-25 23:12:32 +0500

    added recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,7 +17,15 @@ export class RecipeListComponent  implements OnInit{
         "test recipe",
         "this is a test recipe",
      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272"
-    )];
+    ),
+    new Recipe (
+        "test recipe",
+        "this is a test recipe",
+        "https://plus.unsplash.com/premium_photo-1670895801174-8278045808f4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"    ),
+    new Recipe (
+        "test recipe",
+        "this is a test recipe",
+"https://images.unsplash.com/photo-1518779578993-ec3579fee39f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"    ),];
 
     ngOnInit(): void {
     }
